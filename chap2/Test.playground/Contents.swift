1fbdf330904b70dfbccb8c9d54a011714e21e3d5
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2023-11-08 14:37:57 -0800

    First commit

---
+++ chap2/Test.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var mynumber = 2 * 25
+var anothernumber = 8 - 40 * 2
+var myfraction1 = 5.0 / 2.0
+var myfraction2 = 5 / 2.0
+var myfraction = 5 / 2
+var remainder1 = 11 % 3
+var remainder2 = 20 % 8
+var remainder3 = 5 % 2
+
+mynumber += 4
+
+let myconstant = 5
+// myconstant = 4
+
+
+
+
+
+
+
+
+
+
+
